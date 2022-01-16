e039cd3cd6077fc2b292cf66116b1df6fa61f612
glompos21 <glompos21@posteo.net>
2022-01-16 20:26:49 +0200

    remove degug

--- Debug/Core/Src/sys_sensors.d
+++
@@ -1,6 +0,0 @@
-Core/Src/sys_sensors.o: ../Core/Src/sys_sensors.c ../Core/Inc/sys_conf.h \
- ../Core/Inc/sys_sensors.h
-
-../Core/Inc/sys_conf.h:
-
-../Core/Inc/sys_sensors.h:
--- Debug/Core/Startup/startup_stm32wle5ccux.d
+++
@@ -1,2 +0,0 @@
-Core/Startup/startup_stm32wle5ccux.o: \
- ../Core/Startup/startup_stm32wle5ccux.s
